f92f9a5cb0226c28925958f62dc67f2d5d5d7060
Saumit Dinesan <justsaumit@protonmail.com>
2022-09-04 20:06:55 +0530

    Applying font2 patch

--- config.h
+++ config.h
@@ -6,6 +6,12 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "UbuntuMono Nerd Font:pixelsize=22:antialias=true:autohint=true";
+static char *font2[] = {
+    "Spoqa Han Sans Neo:pixelsize=15:antialias=true:autohint=true",
+    "Hack Nerd Font:pixelsize=23:antialias=true:autohint=true",
+    "Joypixels:pixelsize=14:antialias=true:autohint=true"
+};
+
 static int borderpx = 2;
 
 /*
